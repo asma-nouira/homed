e01e10efb6ccf4a144f888ad2fb757a300f8b1a5
asma <asma@viruscomm.ca>
2026-09-22 11:00:29 +0100

    first commit

--- wp-content/themes/vsc-theme/functions.php
+++ wp-content/themes/vsc-theme/functions.php
@@ -187,12 +187,4 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-/**
- * Load WooCommerce compatibility file.
- */
-if ( class_exists( 'WooCommerce' ) ) {
-	require get_template_directory() . '/inc/woocommerce.php';
-}
-
-
 include_once "integrated_vc.php";
