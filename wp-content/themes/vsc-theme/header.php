8d539e134f432e30e4f1d5ed2e3c2a444035c8ea
asma <asma@viruscomm.ca>
2026-09-23 10:22:56 +0100

    qa

--- wp-content/themes/vsc-theme/header.php
+++ wp-content/themes/vsc-theme/header.php
@@ -2,24 +2,6 @@
 /**
  * The header for our theme
  *
- * This is the template that displays all of the <head> section and everything up until <div id="content">
- *
- * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
- *
- * @package vsc-theme
- */
-
-?>
-<!doctype html>
-<html <?php language_attributes(); ?>>
-<head>
-	<meta charset="<?php bloginfo( 'charset' ); ?>">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="profile" href="https://gmpg.org/xfn/11">
-<?php
-/**
- * The header for our theme
- *
  * @package vsc-theme
  */
 
@@ -80,31 +62,6 @@ $vsc_rdv_url   = get_theme_mod( 'vsc_header_rdv_url', '' );
 					<a class="header-btn header-btn--dark" href="<?php echo esc_url( $vsc_rdv_url ); ?>"<?php echo ( false === strpos( $vsc_rdv_url, home_url() ) ) ? ' target="_blank" rel="noopener"' : ''; ?>><?php echo esc_html( $vsc_rdv_label ); ?></a>
 				<?php endif; ?>
 			</div>
-		</nav><!-- #site-navigation -->
-	</header><!-- #masthead -->
-
-	<div id="content" class="site-content">
-	<?php wp_head(); ?>
-</head>
-
-<body <?php body_class(); ?>>
-<div id="page" class="site">
-
-	<header id="masthead" class="site-header">
-		<div class="site-branding">
-			<?php
-			the_custom_logo();
-				?>	
-		</div><!-- .site-branding -->
-
-		<nav id="site-navigation" class="main-navigation">
-			<button class="menu-toggle" aria-controls="primary-menu" aria-expanded="false"><?php esc_html_e( 'Primary Menu', 'vsc-theme' ); ?></button>
-			<?php
-			wp_nav_menu( array(
-				'theme_location' => 'menu-1',
-				'menu_id'        => 'primary-menu',
-			) );
-			?>
 		</nav><!-- #site-navigation -->
 	</header><!-- #masthead -->
 
