057eb78b3a4a6d7f8348f36278c48bb44928f6ac
asma <asma@viruscomm.ca>
2026-09-24 14:36:09 +0100

    home page

--- wp-content/themes/vsc-theme/functions.php
+++ wp-content/themes/vsc-theme/functions.php
@@ -10,47 +10,18 @@
 if ( ! function_exists( 'vsc_theme_setup' ) ) :
 	/**
 	 * Sets up theme defaults and registers support for various WordPress features.
-	 *
-	 * Note that this function is hooked into the after_setup_theme hook, which
-	 * runs before the init hook. The init hook is too late for some features, such
-	 * as indicating support for post thumbnails.
 	 */
 	function vsc_theme_setup() {
-		/*
-		 * Make theme available for translation.
-		 * Translations can be filed in the /languages/ directory.
-		 * If you're building a theme based on vsc-theme, use a find and replace
-		 * to change 'vsc-theme' to the name of your theme in all the template files.
-		 */
 		load_theme_textdomain( 'vsc-theme', get_template_directory() . '/languages' );
 
-		// Add default posts and comments RSS feed links to head.
 		add_theme_support( 'automatic-feed-links' );
-
-		/*
-		 * Let WordPress manage the document title.
-		 * By adding theme support, we declare that this theme does not use a
-		 * hard-coded <title> tag in the document head, and expect WordPress to
-		 * provide it for us.
-		 */
 		add_theme_support( 'title-tag' );
-
-		/*
-		 * Enable support for Post Thumbnails on posts and pages.
-		 *
-		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
-		 */
 		add_theme_support( 'post-thumbnails' );
 
-		// This theme uses wp_nav_menu() in one location.
 		register_nav_menus( array(
 			'menu-1' => esc_html__( 'Primary', 'vsc-theme' ),
 		) );
 
-		/*
-		 * Switch default core markup for search form, comment form, and comments
-		 * to output valid HTML5.
-		 */
 		add_theme_support( 'html5', array(
 			'search-form',
 			'comment-form',
@@ -59,20 +30,13 @@ if ( ! function_exists( 'vsc_theme_setup' ) ) :
 			'caption',
 		) );
 
-		// Set up the WordPress core custom background feature.
 		add_theme_support( 'custom-background', apply_filters( 'vsc_theme_custom_background_args', array(
 			'default-color' => 'ffffff',
 			'default-image' => '',
 		) ) );
 
-		// Add theme support for selective refresh for widgets.
 		add_theme_support( 'customize-selective-refresh-widgets' );
 
-		/**
-		 * Add support for core custom logo.
-		 *
-		 * @link https://codex.wordpress.org/Theme_Logo
-		 */
 		add_theme_support( 'custom-logo', array(
 			'height'      => 250,
 			'width'       => 250,
@@ -84,15 +48,9 @@ endif;
 add_action( 'after_setup_theme', 'vsc_theme_setup' );
 
 /**
- * Set the content width in pixels, based on the theme's design and stylesheet.
- *
- * Priority 0 to make it available to lower priority callbacks.
- *
- * @global int $content_width
+ * Set the content width in pixels.
  */
 function vsc_theme_content_width() {
-	// This variable is intended to be overruled from themes.
-	// Open WPCS issue: {@link https://github.com/WordPress-Coding-Standards/WordPress-Coding-Standards/issues/1043}.
 	// phpcs:ignore WordPress.NamingConventions.PrefixAllGlobals.NonPrefixedVariableFound
 	$GLOBALS['content_width'] = apply_filters( 'vsc_theme_content_width', 640 );
 }
@@ -100,8 +58,6 @@ add_action( 'after_setup_theme', 'vsc_theme_content_width', 0 );
 
 /**
  * Register widget area.
- *
- * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
 function vsc_theme_widgets_init() {
 	register_sidebar( array(
@@ -117,38 +73,69 @@ function vsc_theme_widgets_init() {
 add_action( 'widgets_init', 'vsc_theme_widgets_init' );
 
 /**
+ * Version = date de modification du fichier.
+ * Le navigateur recharge le fichier dès qu'il change (fini les problèmes de cache).
+ */
+function vsc_theme_ver( $path ) {
+	$file = get_template_directory() . $path;
+	return file_exists( $file ) ? (string) filemtime( $file ) : null;
+}
+
+/**
  * Enqueue scripts and styles.
+ *
+ * CSS : dans le <head> (avant le contenu) → pas de « flash » et les animations
+ *       partent bien de l'état caché.
+ * JS  : en pied de page (dernier paramètre à true).
  */
 function vsc_theme_scripts() {
-	wp_enqueue_style('vsc-header-style',get_template_directory_uri().'/css/header.css',array(),null);
-	}
-add_action( 'wp_enqueue_scripts', 'vsc_theme_scripts' );
+	$uri = get_template_directory_uri();
 
+	// ---------- CSS
+	wp_enqueue_style( 'vsc-theme-fonts',  $uri . '/css/fonts/fonts.css', array(), vsc_theme_ver( '/css/fonts/fonts.css' ) );
+	wp_enqueue_style( 'vsc-header-style', $uri . '/css/header.css',      array( 'vsc-theme-fonts' ), vsc_theme_ver( '/css/header.css' ) );
+	wp_enqueue_style( 'vsc-mobile-style', $uri . '/css/mobile.css',      array(), vsc_theme_ver( '/css/mobile.css' ) );
+	wp_enqueue_style( 'vsc-theme-style',  $uri . '/css/style.css',       array(), vsc_theme_ver( '/css/style.css' ) );
+	wp_enqueue_style( 'vsc-animations',   $uri . '/css/animations.css',  array(), vsc_theme_ver( '/css/animations.css' ) );
 
-add_action('wp_footer',function(){
-	wp_enqueue_style('vsc-mobile-style',get_template_directory_uri().'/css/mobile.css',array(),null);
-	wp_enqueue_style('vsc-theme-style',get_template_directory_uri().'/css/style.css',array(),null);
-	wp_enqueue_style('vsc-theme-fonts',get_template_directory_uri().'/css/fonts/fonts.css',array(),null);
-	wp_enqueue_style( 'vsc-animations', get_template_directory_uri(). '/css/animations.css', array(), null);
 	//////////////// Slick Slider CSS ///////////////
-	 wp_enqueue_style('vsc-theme-slick',get_template_directory_uri().'/slick-slider/slick-theme.css',array(),null);
-	wp_enqueue_script( 'vsc-theme-navigation', get_template_directory_uri() . '/js/navigation.js', array(), null, true );
-	wp_enqueue_script( 'vsc-theme-skip-link-focus-fix', get_template_directory_uri() . '/js/skip-link-focus-fix.js', array(), null, true );
-	wp_enqueue_script( 'vsc-reveal', get_template_directory_uri(). '/js/reveal.js', array(), null, true );
-	///////////// slick slider JS ///////////
-	 wp_enqueue_script( 'vsc-slick-js', get_template_directory_uri() . '/slick-slider/slick.min.js', array('jquery'), null, true );
-     wp_enqueue_script( 'vsc-slick-run', get_template_directory_uri() . '/slick-slider/run.js', array('jquery'), null, true );
+	wp_enqueue_style( 'vsc-theme-slick', $uri . '/slick-slider/slick-theme.css', array(), vsc_theme_ver( '/slick-slider/slick-theme.css' ) );
+
+	// ---------- JS (pied de page)
+	wp_enqueue_script( 'vsc-theme-navigation', $uri . '/js/navigation.js', array(), vsc_theme_ver( '/js/navigation.js' ), true );
+	wp_enqueue_script( 'vsc-theme-skip-link-focus-fix', $uri . '/js/skip-link-focus-fix.js', array(), vsc_theme_ver( '/js/skip-link-focus-fix.js' ), true );
+	wp_enqueue_script( 'vsc-reveal', $uri . '/js/reveal.js', array(), vsc_theme_ver( '/js/reveal.js' ), true );
+
+	///////////// Slick Slider JS ///////////
+	wp_enqueue_script( 'vsc-slick-js',  $uri . '/slick-slider/slick.min.js', array( 'jquery' ), vsc_theme_ver( '/slick-slider/slick.min.js' ), true );
+	wp_enqueue_script( 'vsc-slick-run', $uri . '/slick-slider/run.js',       array( 'jquery', 'vsc-slick-js' ), vsc_theme_ver( '/slick-slider/run.js' ), true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
 	}
-});
+}
+add_action( 'wp_enqueue_scripts', 'vsc_theme_scripts' );
 
-// remove wp version param from any enqueued scripts
+/**
+ * Dans le <head>, tout en haut :
+ * 1. Active l'état « caché » des animations AVANT l'affichage (classe vsc-anim sur <html>).
+ *    Filet de sécurité : si reveal.js ne se charge pas, tout redevient visible après 3 s.
+ * 2. Précharge Manrope Regular (police du menu).
+ */
+add_action( 'wp_head', function () {
+	echo "<script>document.documentElement.classList.add('vsc-anim');setTimeout(function(){if(!window.vscReveal){document.documentElement.classList.remove('vsc-anim');}},3000);</script>\n";
+	echo '<link rel="preload" href="' . esc_url( get_template_directory_uri() . '/css/fonts/Manrope/Manrope-Regular.ttf' ) . '" as="font" type="font/ttf" crossorigin>' . "\n";
+}, 0 );
+
+/**
+ * Retire seulement la version de WordPress des URL (sécurité),
+ * en gardant les versions du thème (utiles pour vider le cache).
+ */
 function vc_remove_wp_ver_css_js( $src ) {
-    if ( strpos( $src, 'ver=' ) )
-        $src = remove_query_arg( 'ver', $src );
-    return $src;
+	if ( strpos( $src, 'ver=' . get_bloginfo( 'version' ) ) ) {
+		$src = remove_query_arg( 'ver', $src );
+	}
+	return $src;
 }
 add_filter( 'style_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
 add_filter( 'script_loader_src', 'vc_remove_wp_ver_css_js', 9999 );
@@ -180,4 +167,4 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 	require get_template_directory() . '/inc/jetpack.php';
 }
 
-include_once "integrated_vc.php";
+include_once 'integrated_vc.php';
